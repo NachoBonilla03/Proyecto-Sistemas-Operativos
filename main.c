ba4355ab9b4334588e08c75177f0ad614248be5a
NachoBonilla03 <ignacio.bonilla.rojas@est.una.ac.cr>
2024-04-14 00:07:02 -0600

    Comienzo de main

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int pipefd[2];
+  char buffer[1024];
+  int bytesLeidos;
+  pipe(pipefd);
+  pid_t pid = fork();
+  
+  if(strcmp(argv[1], "-R")==0){
+	  if (pid == 0) {
+	    // Proceso hijo
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    execl("./RAM.o","RAM",  argv[2],argv[3]);
+	    exit(1);
+	   } 
+	  else {
+	    close(pipefd[1]); // Cerrar extremo de escritura
+	    while ((bytesLeidos = read(pipefd[0], buffer[9000], sizeof(buffer))) > 0) {
+	      write(1, buffer, bytesLeidos); // Imprimir la salida en la consola
+	    }
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    wait(NULL);
+	  }
+   }	
+
+   if(strcmp(argv[1], "-C")==0){
+	  if (pid == 0) {
+	    // Proceso hijo
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    execl("./cpu.o","cpu",  argv[2],argv[3]);
+	    exit(1);
+	   } 
+	  else {
+	    close(pipefd[1]); // Cerrar extremo de escritura
+	    while ((bytesLeidos = read(pipefd[0], buffer[9000], sizeof(buffer))) > 0) {
+	      write(1, buffer, bytesLeidos); // Imprimir la salida en la consola
+	    }
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    wait(NULL);
+	  }
+   }
+   
+    if(strcmp(argv[1], "-D")==0){
+	  if (pid == 0) {
+	    // Proceso hijo
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    execl("./disco.o","disco",  argv[2],argv[3]);
+	    exit(1);
+	   } 
+	  else {
+	    close(pipefd[1]); // Cerrar extremo de escritura
+	    while ((bytesLeidos = read(pipefd[0], buffer[9000], sizeof(buffer))) > 0) {
+	      write(1, buffer, bytesLeidos); // Imprimir la salida en la consola
+	    }
+	    close(pipefd[0]); // Cerrar extremo de lectura
+	    wait(NULL);
+	  }
+   }
+
+  return 0;
+}
