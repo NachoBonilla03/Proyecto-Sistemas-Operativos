fc8d32dd2a611717896de07f155a0c6250e8885c
VDMarcos <marcos.vasquez.diaz@est.una.ac.cr>
2024-04-14 11:48:56 -0600

    little change in the main

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]) {
   int bytesLeidos;
   pipe(pipefd);
   pid_t pid = fork();
+
+  if(argc>3){
+	printf("Error: Too many arguments RTFM\n");
+	return 0;
+  }
   
   if(strcmp(argv[1], "-R")==0){
 	  if (pid == 0) {
@@ -30,7 +35,12 @@ int main(int argc, char *argv[]) {
 	  if (pid == 0) {
 	    // Proceso hijo
 	    close(pipefd[0]); // Cerrar extremo de lectura
-	    execl("./cpu","cpu",  argv[2], argv[3]);
+		if(argc == 2){
+			execl("./cpu","cpu",  argv[2]);
+		}
+		if(argc==3){
+			execl("./cpu","cpu",  argv[2], argv[3]);
+		}
 	    exit(1);
 	   } 
 	  else {
